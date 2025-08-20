eb285693e4b46699cdd8dc0fd8c24e5c6046a3a0
CraftLOL12 <134438564+CraftLOL12@users.noreply.github.com>
2025-08-20 10:02:00 +0300

    Add dist-electron/server/routes/settingsRoutes.js 1755673317992

---
+++ dist-electron/server/routes/settingsRoutes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { getSettingsController, updateSettingsController, } from "../controllers/index.js";
+export const settingsRoutes = Router();
+/* Settings */
+settingsRoutes.get("/settings", getSettingsController);
+settingsRoutes.put("/settings", updateSettingsController);

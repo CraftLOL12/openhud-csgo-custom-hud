7f686700eb1021546460c9668ca9012c244e3d53
CraftLOL12 <134438564+CraftLOL12@users.noreply.github.com>
2025-08-20 10:01:49 +0300

    Add dist-electron/main.js 1755673308079

---
+++ dist-electron/main.js
@@ -0,0 +1,59 @@
+import { app, BrowserWindow } from "electron";
+import { checkDirectories, isDev, showNotification, getPreloadPath, getUIPath, } from "./helpers/index.js";
+import { shutDown, startServer } from "./server/server.js";
+import { createTray } from "./tray.js";
+import { createMenu } from "./menu.js";
+import { ipcMainEvents } from "./ipcEvents/index.js";
+let server;
+let mainWindow;
+app.on("ready", () => {
+    mainWindow = createMainWindow();
+    checkDirectories();
+    server = startServer(mainWindow);
+    createTray(mainWindow);
+    createMenu(mainWindow);
+    handleCloseEvents(mainWindow);
+    ipcMainEvents(mainWindow);
+});
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        minWidth: 800,
+        height: 700,
+        minHeight: 513,
+        frame: false,
+        webPreferences: {
+            preload: getPreloadPath(),
+        },
+    });
+    if (isDev()) {
+        mainWindow.loadURL("http://localhost:5123");
+    }
+    else {
+        mainWindow.loadFile(getUIPath());
+    }
+    return mainWindow;
+}
+function handleCloseEvents(mainWindow) {
+    /* Handle minimizing to tray */
+    let willClose = false;
+    mainWindow.on("close", (e) => {
+        if (willClose) {
+            return;
+        }
+        e.preventDefault();
+        showNotification("Application still running but minimized to tray");
+        mainWindow.hide();
+        if (app.dock) {
+            app.dock.hide();
+        }
+    });
+    // Reset willClose when we open the app from the tray again
+    app.on("before-quit", () => {
+        willClose = true;
+        shutDown(server);
+    });
+    mainWindow.on("show", () => {
+        willClose = false;
+    });
+}
